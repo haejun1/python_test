65a0bea234811365c6b0b42de26849e8b92e5573
Baekhaejun <a01027833227@gmail.com>
2025-09-12 23:10:43 +0900

    BFS간단하게

--- highlevPython.py
+++ highlevPython.py
@@ -1,16 +1,44 @@
 #1. DFS 재귀함수
+# graph = {
+#     1: [2, 3],
+#     2: [1, 4],
+#     3: [1],
+#     4: [2]
+# }
+# visited = set()
+# def dfs(node):
+#     if node in visited:
+#         return
+#     visited.add(node)
+#     print(node, end=" ")
+#     for nxt in graph[node]:
+#         dfs(nxt)
+# dfs(4)  # 4 2 1 3
+
+#2-1. BFS
+from collections import deque
+
+def bfs(start):
+    queue = deque([start])   # 큐 초기화
+    visited = set([start])   # 방문 기록 (중복 방지)
+
+    print(f"시작 노드: {start}")
+
+    while queue:
+        node = queue.popleft()   # 큐에서 꺼내기
+        print(node, end=" ")
+
+        for nxt in graph[node]:  # 인접 노드 확인
+            if nxt not in visited:
+                visited.add(nxt)
+                queue.append(nxt)
 graph = {
     1: [2, 3],
-    2: [1, 4],
-    3: [1],
-    4: [2]
+    2: [4, 5],
+    3: [6],
+    4: [],
+    5: [],
+    6: []
 }
-visited = set()
-def dfs(node):
-    if node in visited:
-        return
-    visited.add(node)
-    print(node, end=" ")
-    for nxt in graph[node]:
-        dfs(nxt)
-dfs(1)  
+bfs(1)
+
